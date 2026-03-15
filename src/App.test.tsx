fd1e95b0e7db5459cc4a6f840a0ea4643a2a517b
Sadikshya <sadikshyeah@gmail.com>
2026-03-15 23:07:58 +0200

    test for button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,14 @@
 
 import App from "./App";
 import { test , expect} from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom/vitest';
 
 test("renders header", () => {
   render(<App />);
   const header = screen.getByText(/My Todos/i);
   expect(header).toBeInTheDocument();
+
+  const button = screen.getByText("Add");
+  fireEvent.click(button);
 });
