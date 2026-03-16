ebe8232c369d9c2cbf98ea768d944f7bd13a5e9b
Sadikshya <sadikshyeah@gmail.com>
2026-03-16 21:49:41 +0200

    test

--- src/Todolist.test.tsx
+++ src/Todolist.test.tsx
@@ -1,7 +1,26 @@
 import TodoList from "./TodoList";
-import { test } from "vitest";
-import { render } from "@testing-library/react";
+import { test, afterEach, expect } from "vitest";
+import { cleanup, render, screen, fireEvent } from "@testing-library/react";
+import '@testing-library/jest-dom/vitest';
+
 
 test("add todo", () => {
   render(<TodoList />);
+
+  const desc = screen.getByPlaceholderText("Description");
+  fireEvent.change(desc, { target: { value: "Go to coffee" } });
+  const date = screen.getByPlaceholderText("Date");
+  fireEvent.change(date, { target: { value: "20.11.2025" } });
+  
+  const button = screen.getByText("Add");
+  fireEvent.click(button);
+
+  expect(screen.getByRole("table")).toHaveTextContent(/go to coffee/i);
 });
+
+afterEach(() => {
+  cleanup();
+});
+
+
+
